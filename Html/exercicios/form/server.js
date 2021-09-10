0aee3c440dbf4aca2e6c81775d3918c3aebd75af
M-A-Saldanha <72634705+M-A-Saldanha@users.noreply.github.com>
2021-09-10 01:25:23 -0300

    Curso de Web Moderno 2021

--- Html/exercicios/form/server.js
+++ Html/exercicios/form/server.js
@@ -7,7 +7,14 @@ app.use(express.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
+    console.log(req.query)
     resp.send('<h1>Parabéns</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuario alterado!</h1>')
 })
 
 app.listen(3003)
